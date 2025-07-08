875b1fa058be7fb5c38f3e56725cc1c456237357
matesp3 <148990870+matesp3@users.noreply.github.com>
2025-07-09 00:21:54 +0200

    Final v1

--- work/DAV-eng-v.sql
+++ work/DAV-eng-v.sql
@@ -13,16 +13,16 @@ SELECT * -- finding out whether alcohol was present during accident or not
  FROM cr_nehody acc
  JOIN cr_pritomnost_alko alco ON acc.id_alko_prit = alco.id_stav;
 
-SELECT	id_stav AS state_id, -- presence OF alcohol - states
-		pritomny AS present,
-		CASE pritomny
+SELECT	alco.id_stav AS state_id, -- presence OF alcohol - states
+		alco.pritomny AS present,
+		CASE alco.pritomny
         WHEN 'A' THEN 'Yes'
         WHEN 'N' THEN 'No'
         WHEN 'O' THEN 'Refused'
         WHEN 'X' THEN 'Not observed'
     END AS was_present,
-    OBSAH_PERC AS amount_perc
-  FROM CR_PRITOMNOST_ALKO
+    per_mille AS amount_perc
+  FROM CR_PRITOMNOST_ALKO alco
    ORDER BY was_present desc NULLS LAST, state_id;
 
 SELECT	'Alco-ratio (accidents): ' || round(100*alco_acc_cnt/accidents_count, 2) || '%' 
@@ -55,7 +55,7 @@ select case when alco.pritomny='A' then 'Yes'
             when alco.pritomny='O' then 'Refused'
             when alco.pritomny='X' then 'Not observed'
             else 'Other' end as alco_present,
-    alco.obsah_perc as alco_value_in_blood,
+    alco.per_mille as alco_value_in_blood,
     count(*) as accidents_count,
     round(count(*)/
     (SELECT count(*) AS overall_cnt FROM cr_nehody WHERE EXTRACT(YEAR FROM cas)=2024)
@@ -63,7 +63,7 @@ select case when alco.pritomny='A' then 'Yes'
  from cr_nehody n
  join cr_pritomnost_alko alco on n.id_alko_prit = alco.id_stav
   where extract(year from cas)=2024
-   group by alco.id_stav, alco.pritomny, alco.obsah_perc
+   group by alco.id_stav, alco.pritomny, alco.per_mille
     order by accidents_count DESC;
   
 -- monthly ratio of accidents with confirmed alcohol usage among all accidents
@@ -161,11 +161,13 @@ SELECT month_id, accidents
 
 --===================================================================================================
 -- task 1: what's the percentage ratio of the most devastating accidents damage with alcohol usage among TOP 1000 most devastating accidents of any kind
-select damage_alco, damage_overall, round(100 * damage_alco/damage_overall, 2) || '%' as perc_ratio from (
- select
- (select 1 from dual) as damage_alco,
- (select 2 from dual) as damage_overall
-  from dual
+select damage_alco, damage_overall, 
+round(100 * damage_alco/damage_overall, 2) || '%' as perc_ratio
+ from (
+	 select
+	 (select 1 from dual) as damage_alco,
+	 (select 2 from dual) as damage_overall
+	 from dual
 );
 
 -- v1 - overall damage computed from 1000 biggest accidents of any kind
@@ -213,19 +215,22 @@ select sum(damage) damage_alco    --v2(aggregate) cost: 1260
 -- from first 1000 most costly accidents, how much alcohol accidents participate in overall damage
 -- Overally, we have over 90000 accidents, so we are working just with 1.1 % of all in 2024
 -- cost: 1531
-select 'Share of alcohol damage among TOP 1000 most costly damages:' as description,
- round(100*(damage_alco/damage_overall),3) || '%' as perc, damage_alco, damage_overall from ( 
-  select 
-    (select sum(damage) damage_alc    --v2(aggregate) cost: 766
+select 'Share of alcohol-related accidents damage among TOP 
+	    1000 most costly damages:' as description,
+	    round(100*(damage_alco/damage_overall),3) || '%' as perc,
+	    damage_alco, damage_overall 
+ from (select 
+    (select sum(damage)
       from (
-        select alco.pritomny as alco_present, acc.celk_skoda_kc as damage,
-            row_number() over(order by acc.celk_skoda_kc desc) as rn
+        	select alco.pritomny as alco_present,
+        		   acc.celk_skoda_kc as damage,
+           	   	   row_number() over(order by acc.celk_skoda_kc desc) as rn
           from cr_nehody acc
            join cr_pritomnost_alko alco on acc.id_alko_prit = alco.id_stav
             where extract(year from acc.cas)=2024
            ) where rn <= 1000 and alco_present='A'
     ) AS damage_alco, 
-    (select sum(damage) damage_ovrl --v2(aggregate) cost: 763
+    (select sum(damage)
       from (
         select celk_skoda_kc as damage,
             row_number() over(order by acc.celk_skoda_kc desc) as rn
@@ -233,13 +238,12 @@ select 'Share of alcohol damage among TOP 1000 most costly damages:' as descript
            where extract(year from acc.cas)=2024
            ) where rn <= 1000
      ) AS damage_overall
-   from dual);
+ 	from dual);
    
 --===================================================================================================
 -- 2.5-month moving median of amount of injured (slightly + seriously) for 2024 (frequency = half-month)
 -- cost: 816
-with 
- month_nr as (
+with month_nr as (
     select 1 as month_id from dual
     union select 2 from dual
     union select 3 from dual
@@ -254,36 +258,45 @@ with
     union select 12 from dual
  ),
  months as (
-     select month_id, to_date('2024-'||month_id||'-01', 'yyyy-mm-dd') as month_bt 
+	select month_id,
+	to_date('2024-'||month_id||'-01', 'yyyy-mm-dd') as month_bt 
       from month_nr
  ),
  halfmonths as (
     select month_bt as halfmon from months
      union
-    select month_bt + floor((last_day(month_bt)-month_bt+1)/2) as month_half from months
+    select 
+    month_bt + floor((last_day(month_bt)-month_bt+1)/2) as halfmon 
+	 from months
  ),
  halfmon_data as (
  select halfmon, sum(count_injured) as count_injured
  from (
      select halfmon, count_injured
       from (
-         select acc.id_nehoda, trunc(acc.cas)as date_trunc, hms.halfmon, (acc.lahko_zraneni+acc.tazko_zraneni) as count_injured, 
+         select acc.id_nehoda, hms.halfmon,
+          (acc.lahko_zraneni + acc.tazko_zraneni) as count_injured, 
           row_number() over (partition by id_nehoda order by 
-           (case when trunc(acc.cas)-hms.halfmon>=0 then trunc(acc.cas)-hms.halfmon else 10000 end)) as hms_priority
+           (case when trunc(acc.cas)-hms.halfmon>=0 
+            then trunc(acc.cas)-hms.halfmon else 10000 end)) as hms_priority
           from cr_nehody acc
            join halfmonths hms on trunc(acc.cas, 'MM')=trunc(hms.halfmon, 'MM')
-            where extract(year from acc.cas)=2024
+           join cr_pritomnost_alko alco on acc.id_alko_prit = alco.id_stav
+            where extract(year from acc.cas)=2024 AND alco.pritomny LIKE 'A'
       ) where hms_priority=1 -- each record is going to have assigned 1 halfmon, so the number of halfmonths = count(*)
- )
-  group by halfmon -- development of injuries number with frequency equal to halfmon
+ 	) group by halfmon -- development of injuries number with frequency equal to halfmon
  )
 select halfmon, median(count_injured) median_injured
  from (
     select halfmon, count_injured from halfmon_data
-     union all select halfmon, lag(count_injured,1) over(order by halfmon) from halfmon_data
-     union all select halfmon, lag(count_injured,2) over(order by halfmon) from halfmon_data
-     union all select halfmon, lag(count_injured,3) over(order by halfmon) from halfmon_data
-     union all select halfmon, lag(count_injured,4) over(order by halfmon) from halfmon_data
+     union all select halfmon, 
+     lag(count_injured, 1) over(order by halfmon) from halfmon_data
+     union all select halfmon, 
+     lag(count_injured, 2) over(order by halfmon) from halfmon_data
+     union all select halfmon, 
+     lag(count_injured, 3) over(order by halfmon) from halfmon_data
+     union all select halfmon, 
+     lag(count_injured, 4) over(order by halfmon) from halfmon_data
 ) group by halfmon order by halfmon; 
 
 
@@ -375,14 +388,15 @@ SELECT * FROM
  ORDER BY pritomny;
 
 -- in which hours has occured most accidents related to alcohol
--- TODO: histogram
-SELECT	count(*) AS cnt,
-		EXTRACT(HOUR FROM acc.cas) AS iHour
+SELECT	count(*) AS accidents,
+		EXTRACT(HOUR FROM acc.cas) AS hr
  FROM cr_nehody acc
  JOIN cr_pritomnost_alko alco ON acc.id_alko_prit = alco.id_stav
   WHERE EXTRACT(YEAR FROM acc.cas)=2024 AND alco.pritomny LIKE 'A'
   GROUP BY EXTRACT(HOUR FROM acc.cas)
-   ORDER BY cnt desc;
+   ORDER BY hr;
+  
+  
 -- okej, zistili sme, ze vo vecernych hodinach, ale deje sa to aj cez tyzden alebo skor cez vikend
   
 SELECT	to_char(acc.cas, 'D'),
@@ -394,7 +408,6 @@ SELECT	count(*) AS cnt,
 		to_char(acc.cas, 'Day') AS dd
  FROM cr_nehody acc
  JOIN cr_pritomnost_alko alco ON acc.id_alko_prit = alco.id_stav
---  WHERE EXTRACT(YEAR FROM acc.cas)=2024 AND EXTRACT(HOUR FROM acc.cas) IN (18,19,20) AND alco.pritomny LIKE 'A'
   WHERE EXTRACT(YEAR FROM acc.cas)=2024 AND alco.pritomny LIKE 'A'
   GROUP BY to_char(acc.cas, 'Day')
    ORDER BY cnt desc;
@@ -404,19 +417,16 @@ FROM
 (
 	SELECT	region_id, region_name, cnt, ihour,
 			row_number() OVER (PARTITION BY region_id ORDER BY cnt desc) AS rnk
-	FROM
-	(
-		SELECT	regions.id_kraj AS region_id,
-				regions.nazov AS region_name,
-				count(*) AS cnt,
-				EXTRACT(HOUR FROM acc.cas) AS iHour
+	FROM (
+		SELECT	regions.id_kraj AS region_id, regions.nazov AS region_name,
+				count(*) AS cnt, EXTRACT(HOUR FROM acc.cas) AS iHour
 		 FROM cr_nehody acc
 		 JOIN cr_pritomnost_alko alco ON acc.id_alko_prit = alco.id_stav
 		 JOIN cr_kraje regions ON acc.id_kraj = regions.id_kraj
 		  WHERE EXTRACT(YEAR FROM acc.cas)=2024 AND alco.pritomny LIKE 'A'
 		  GROUP BY EXTRACT(HOUR FROM acc.cas), regions.id_kraj, regions.nazov
 		   ORDER BY cnt DESC
-	)
+		)
 ) WHERE rnk <= 3
   ORDER BY rnk, region_id;
  
@@ -439,7 +449,7 @@ SELECT vehicles_per_acc, nr_of_cases, RANK() OVER (ORDER BY nr_of_cases desc) AS
 
 -- average amount of cars on accident with alcohol usage
 WITH vehicles_amount AS (
-	SELECT vehicles_per_acc, count(*) AS nr_of_cases
+	SELECT vehicles_per_acc, count(*) AS accidents
 	 FROM
 	(
 		SELECT acc.poc_vozidiel AS vehicles_per_acc
@@ -448,6 +458,94 @@ WITH vehicles_amount AS (
 		  WHERE EXTRACT(YEAR FROM acc.cas)=2024 AND alco.pritomny LIKE 'A'
 	) GROUP BY vehicles_per_acc
 )
-SELECT round(sum(vehicles_per_acc*nr_of_cases)/sum(nr_of_cases), 3) || ' vehicles per accident with alcohol in 2024' AS wavg
+SELECT round(sum(vehicles_per_acc*accidents)/sum(accidents), 3) AS wavg,
+ 		max(vehicles_per_acc) AS M4X
  FROM vehicles_amount;
 
+SELECT median(acc.poc_vozidiel) AS MEDI4N
+   FROM cr_nehody acc;
+   
+SELECT coa.popis, toa.popis
+	FROM CR_NEHODY acc
+	JOIN CR_CHARAKTER_NEHODY coa ON acc.id_char_nehod = coa.id_charakter
+	JOIN CR_DRUH_NEHODY toa ON acc.id_druh_nehody = toa.id_druh
+	 WHERE acc.poc_vozidiel = 1;
+
+------------------------------------------------------------------------
+-- 			INDEXES - OPTIMIZATION OF QUERIES
+------------------------------------------------------------------------
+SELECT index_name, index_type, uniqueness, status
+FROM all_indexes
+WHERE table_name = 'CR_NEHODY';
+--WHERE table_name = 'CR_PRITOMNOST_ALKO';
+
+--DROP INDEX IDX_BTREE_NEHODY_ROK_DRUH;
+--DROP INDEX IDX_BTREE_NEH_DRUH_NEHODY;
+--DROP INDEX IDX_BTREE_NEH_YEAR;
+
+SELECT aic.index_name, aic.column_name, aic.column_position, ai.index_type, ai.uniqueness
+FROM all_ind_columns aic
+JOIN all_indexes ai ON aic.index_name = ai.index_name AND aic.table_owner = ai.owner
+WHERE aic.table_name = 'CR_PRITOMNOST_ALKO'
+ORDER BY aic.index_name, aic.column_position;
+
+select ui.table_name, ui.index_name, ui.index_type, ui.uniqueness, ui.status, ui.visibility, 
+       uc.constraint_type, uc.owner, uc.constraint_name, uc.status, uc.generated, uc.index_name
+ from user_indexes ui
+  left join user_constraints uc on ui.index_name=uc.constraint_name
+  where ui.table_name='CR_NEHODY'; -- SYS_C00197691
+  -- ctrl-shift-e for displaying execution plan
+
+ -- to here, just PK indexes on 'acc' and 'alco' table
+--create bitmap index idx_acc_alco_type_bitmap on cr_nehody(id_alko_prit);
+--DROP INDEX idx_acc_alco_type_bitmap;
+ -- original cost:		772
+ -- bitmap index cost:	772
+--create index idx_acc_alco_type_btree on cr_nehody(id_alko_prit);
+--DROP INDEX idx_acc_alco_type_btree;
+
+  SELECT DENSE_RANK() OVER (ORDER BY wavg_ratio desc) AS ranking,
+      region_name, round(wavg_ratio, 3) || '%' wavg_ratio, round(avg_ratio, 3) || '%' avg_ratio
+ FROM 
+(
+	SELECT id_region, region_name,
+		avg((alco_amount/overall_amount)*100) AS avg_ratio,
+		sum(days_in_month * (alco_amount/overall_amount)*100/365) AS wavg_ratio
+	FROM
+	(
+		SELECT 
+		 id_region,
+		 region_name,
+		 month_id,
+		 max(CASE WHEN amount_type_id = 1 THEN accidents ELSE NULL END) AS alco_amount,
+		 max(CASE WHEN amount_type_id = 2 THEN accidents ELSE NULL END) AS overall_amount,
+		 extract(DAY FROM last_day(to_date('2024-' || month_id || '-01', 'YYYY-MM-DD'))) AS days_in_month
+		 FROM
+		(
+			(SELECT 1 AS amount_type_id, count(*) AS accidents, -- alco amounts
+			 regions.id_kraj AS id_region, regions.nazov AS region_name, extract(MONTH FROM acc.cas) month_id 
+			 from cr_nehody acc
+			 JOIN CR_KRAJE regions ON acc.id_kraj = regions.id_kraj
+			 JOIN CR_PRITOMNOST_ALKO alco ON acc.id_alko_prit = alco.id_stav
+			  WHERE EXTRACT(YEAR FROM acc.cas) = 2024 AND alco.pritomny LIKE 'A'
+			   GROUP BY regions.id_kraj, regions.nazov, extract(MONTH FROM cas))
+			UNION ALL
+			(SELECT 2 AS amount_type_id, count(*) AS accidents, -- overall amounts
+			 regions.id_kraj AS id_region, regions.nazov AS region_name, extract(MONTH FROM acc.cas) month_id 
+			 from cr_nehody acc
+			 JOIN CR_KRAJE regions ON acc.id_kraj = regions.id_kraj
+			  WHERE EXTRACT(YEAR FROM acc.cas) = 2024
+			   GROUP BY regions.id_kraj, regions.nazov, extract(MONTH FROM acc.cas))
+		) GROUP BY id_region, region_name, month_id
+	) GROUP BY id_region, region_name
+	  ORDER BY wavg_ratio
+);
+
+--create index idx_btree_nehody_rok_druh on cr_nehody(extract(year from cas), id_druh_nehody); -- cost: 32
+
+SELECT distinct extract(YEAR FROM cas), count(*)
+ FROM cr_nehody GROUP BY extract(YEAR FROM cas);
+create index idx_acc_year_btree on cr_nehody(extract(year from cas));
+DROP INDEX idx_acc_year_btree;
+ -- original cost:		1538
+ -- b-tree index cost:	??	
